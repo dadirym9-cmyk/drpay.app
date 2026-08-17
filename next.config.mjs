ff9186dd2a1c66ddb8d7990a2204e4f6e8a510ea
dadirym9-cmyk <dadirym9@gmail.com>
2026-08-17 07:29:01 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add any specific nextjs config here if needed
-  reactStrictMode: false, // Often better for games during development
+  output: 'export',
+
+  reactStrictMode: false,
+
   transpilePackages: ['phaser'],
-  allowedDevOrigins: ['**.*.*'],
+
+  allowedDevOrigins: ['*.*.*'],
+
   env: {
     PROJECT_ID: process.env.HAPPYSEEDS_PROJECT_ID ?? '',
     REACTUS_BASE_URL: process.env.REACTUS_BASE_URL ?? '',
